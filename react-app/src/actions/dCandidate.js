50c4bf35dc1fd33667f9f104d714035077b664b3
PankornSupercool_PC <47814115+sepankorn@users.noreply.github.com>
2021-04-11 05:06:25 +0700

    Update Backend

--- react-app/src/actions/dCandidate.js
+++ react-app/src/actions/dCandidate.js
@@ -1,4 +1,6 @@
-export const ACTION_TYPES ={
+import api from "./api";
+
+export const ACTION_TYPES = {
     CREATE: 'CREATE',
     UPDATE: 'UPDATE',
     DELETE: 'DELETE',
@@ -8,9 +10,13 @@ export const ACTION_TYPES ={
 
 
 export const fetchAll = () => dispatch => {
+    api.dCandidate()
+        .then(response => {
+            dispatch({
+                type: ACTION_TYPES.FETCH_ALL,
+                payload: response.data
+            })
+        })
+        .catch(err => console.log(err))
     // get  api req 
-    dispatch({
-        type: ACTION_TYPES.FETCH_ALL,
-        payload: []
-    })
 }
--- react-app/src/components/DCandidates.js
+++ react-app/src/components/DCandidates.js
@@ -1,3 +1,7 @@
+import React from "react";
+import {connect} from "react-redux";
+import * as actions from "../actions/dCandidate";
+
 const DCandidates = (props) => {
     return ( 
         <div>
@@ -5,5 +9,13 @@ const DCandidates = (props) => {
         </div>
      );
 }
- 
-export default DCandidates;
+const mapStateToProps = state =>({
+    dCandidateList: state.dCandidate.list
+}) 
+
+const mapActionToProps ={
+    fetchAllDCandidates: actions.fetchAll
+}
+
+
+export default connect(mapStateToProps)(DCandidates);
